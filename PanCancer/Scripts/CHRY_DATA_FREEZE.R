827bbf873e7e909e60326e6e5342b2c8c8a781ed
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-08-25 21:19:13 +0200

    new data

--- PanCancer/Scripts/CHRY_DATA_FREEZE.R
+++ PanCancer/Scripts/CHRY_DATA_FREEZE.R
@@ -4,19 +4,20 @@
 # use_for_patient_level (T/F): column indicating which of the tumor samples to use for patient-level analyses. Relevant when a patient has multiple biopsies sequenced
 # use_for_sample_level (T/F): column indicating whether sample passed basic purity check.
 
-pat_df = read.delim("~/Downloads/mskimpact_clinical_data (65).tsv")
+pat_df = read.delim("~/Documents/GitHub/Y_chromosome_loss/PanCancer/Data_in/IMPACT_clinical_annotation.tsv")
 pat_df = pat_df %>% 
   mutate_if(is.factor, as.character) 
-samp_df <- read.delim("~/Desktop/Projects/40K_Analysis/data/data_clinical_sample.oncokb.txt")
+samp_df <- read.delim("~/Documents/GitHub/Y_chromosome_loss/PanCancer/Data_in/data_clinical_sample.oncokb.txt")
 samp_df <- samp_df %>% 
   mutate_if(is.factor, as.character) %>%
   filter(use_for_patient_level == TRUE)
 samp_df 
 
-data_freeze <- samp_df %>% 
-  dplyr::select(PATIENT_ID, SAMPLE_ID, CANCER_TYPE, facets_qc,TUMOR_PURITY,SAMPLE_TYPE,AGE_AT_SEQ_REPORTED_YEARS,GENE_PANEL,SAMPLE_COVERAGE) %>%
-  left_join(select(pat_df, PATIENT_ID, Sex), by = "PATIENT_ID")%>%
-  filter(Sex != "",Sex !="Female", facets_qc == "TRUE" )%>% distinct()
+data_freeze = samp_df %>% 
+  dplyr::select(PATIENT_ID, SAMPLE_ID, CANCER_TYPE, facets_qc, TUMOR_PURITY, 
+                SAMPLE_TYPE, AGE_AT_SEQ_REPORTED_YEARS, GENE_PANEL, SAMPLE_COVERAGE) %>%
+  left_join(select(pat_df, PATIENT_ID, Sex), by = "PATIENT_ID") %>%
+  filter(Sex != "", Sex != "Female", facets_qc == "TRUE" ) %>% distinct()
 
 write.table(data_freeze, "~/Downloads/data_freeze_cancer_type_082321.txt", sep = "\t", row.names = F, quote = F)
 
@@ -30,6 +31,15 @@ Facets_Countsfile = Facets_annotation[, c('tumor_sample', 'counts_file')]
 data_freeze1 = merge(data_freeze1, Facets_Countsfile, by.x = 'SAMPLE_ID', by.y = 'tumor_sample', all.x = T)
 data_freeze1 = data_freeze1[-which(duplicated(data_freeze1$SAMPLE_ID)), ]
 
+#' WES countsfile: working with n = 957
+WES_data = data_WES[which(data_WES$DMP_Sample_ID %in% data_freeze1$SAMPLE_ID), ]
+
+
+
 #' save output; for generell purpose
-Y_chromosome_cohort_data = list(IMPACT.cohort = data_freeze1)
+Y_chromosome_cohort_data = list(IMPACT.cohort = data_freeze1,
+                                WES.cohort = WES_data)
 saveRDS(object = Y_chromosome_cohort_data, file = '~/Documents/GitHub/Y_chromosome_loss/PanCancer/Data_out/cohort_data.rds')
+
+a = readRDS(file = '~/Documents/GitHub/Y_chromosome_loss/PanCancer/Data_out/cohort_data.rds')
+length(a$WES.cohort$Facet_Countfile)
