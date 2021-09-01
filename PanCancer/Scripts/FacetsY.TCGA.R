c294083762e9919eae4a59c24b93610e1536d81a
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-09-01 16:02:06 +0200

    modi

--- PanCancer/Scripts/FacetsY.TCGA.R
+++ PanCancer/Scripts/FacetsY.TCGA.R
@@ -5,36 +5,6 @@
 ## chris-kreitzer
 
 
-#' ## Libraries and Input
-#' TCGA_clinical_annotation = read.csv('Data_in/TCGA_Clinical_Annotation.txt', sep = '\t')
-#' TCGA_filepaths = read.csv('Data_in/TCGA_filepaths.txt', sep = '\t', header = F)
-#' TCGA_filepaths$sample_id = substr(TCGA_filepaths$V1, start = 1, stop = 12)
-#' 
-#' ## Processing
-#' TCGA_male = TCGA_clinical_annotation[which(TCGA_clinical_annotation$gender == 'MALE'), c('bcr_patient_barcode', 'age_at_initial_pathologic_diagnosis', 'gender') ]
-#' TCGA_cohort = TCGA_male[which(TCGA_male$bcr_patient_barcode %in% TCGA_filepaths$sample_id), ]
-#' 
-#' #' use the first TCGA sample; regardless of number of samples
-#' #' discard all additional samples
-#' TCGA_filepaths$keep = NA
-#' for(i in unique(TCGA_filepaths$sample_id)){
-#'   if(length(TCGA_filepaths$sample_id[which(TCGA_filepaths$sample_id == i)]) > 1){
-#'     TCGA_filepaths[which(TCGA_filepaths$sample_id == i), 'keep'][1] = 'keep'
-#'     TCGA_filepaths[which(TCGA_filepaths$sample_id == i), 'keep'][2:length(TCGA_filepaths$sample_id[which(TCGA_filepaths$sample_id == i)])] = 'discard'
-#'   } else {
-#'     TCGA_filepaths[which(TCGA_filepaths$sample_id == i), 'keep'] = 'keep'
-#'   }
-#' }
-#' 
-#' TCGA_filepaths = TCGA_filepaths[which(TCGA_filepaths$keep %in% 'keep'), ]
-#' TCGA_cohort = merge(TCGA_cohort, TCGA_filepaths, by.x = 'bcr_patient_barcode', by.y = 'sample_id', all.x = T)
-#' 
-#' 
-#' #' create processing Y-chromosome loss pipeline; n = 4,875 male samples TCGA
-#' TCGA_cohort$path = paste0('/juno/work/ccs/tcga_facets/snp-pileup/', TCGA_cohort$V1)
-#' TCGA_cohort$V1 = NULL
-#' write.table(TCGA_cohort, file = 'Data_in/TCGA_paths.txt', sep = '\t', row.names = F, quote = F)
-
 
 ###############################################################################
 # install local FacetsY and pctGCdata
@@ -49,8 +19,6 @@ library(data.table)
 TCGA.samples = read.csv('/juno/home/kreitzec/Y_chromosome_loss/Data/TCGA_paths.txt', sep = '\t')
 TCGA.samples = as.character(TCGA.samples$path)
 
-
-TCGA.samples = TCGA.samples[1:2]
 
 #' function for data conversion to IGV 
 format_igv_seg = function(facets_output, sample_id, normalize = TRUE) {
