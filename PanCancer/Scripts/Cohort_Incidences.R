2f0e649dd153dd99deea3649024881c52009768c
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-09-02 15:54:10 +0200

    new

--- PanCancer/Scripts/Cohort_Incidences.R
+++ PanCancer/Scripts/Cohort_Incidences.R
@@ -107,6 +107,7 @@ for(i in unique(names(IMPACT_top_20))){
 }
 
 IMPACT_incidence$mergedCancerType = paste0(IMPACT_incidence$CancerType, ' (n=', IMPACT_incidence$mergedCancerType, ')')
+write.table(IMPACT_incidence, file = 'Data_out/IMPACT/IMPACT_Y.loss_incidence.txt', sep = '\t', row.names = F, quote = F)
 
 #' make the visualization
 IMPACT_incidence_plot = ggplot(IMPACT_incidence, 
@@ -139,22 +140,72 @@ ggsave_golden(IMPACT_incidence_plot, filename = 'Figures/IMPACT_Y_Loss.pdf', wid
 ###############################################################################
 ###############################################################################
 #' look at the age distribution, and whether age may be associated with higher Y chromosome loss
+IMPACT_data # raw
+IMPACT_data$AGE_AT_SEQ_REPORTED_YEARS = as.numeric(as.character(IMPACT_data$AGE_AT_SEQ_REPORTED_YEARS))
+
+plot_list = list()
+for(i in unique(names(IMPACT_top_20))){
+  data.plot = IMPACT_data[which(IMPACT_data$CANCER_TYPE == i), ]
+  type.density = density(data.plot$AGE_AT_SEQ_REPORTED_YEARS, na.rm = T)
+  median.age = type.density$x[which.max(type.density$y)]
+  print(median.age)
+  peak.max = type.density$y[which.max(type.density$y)]
+  #median.age = median(data.plot$AGE_AT_SEQ_REPORTED_YEARS, na.rm = T)
+  upper.limit.plot = peak.max / 15
+  
+  plot = ggplot(data.plot, aes(x = AGE_AT_SEQ_REPORTED_YEARS)) + 
+    geom_density(size = 1, bw = 'SJ') +
+    theme_minimal() +
+    theme(panel.grid = element_blank(),
+          axis.title = element_blank(),
+          axis.text.y = element_blank(),
+          aspect.ratio = 1) +
+    scale_y_continuous(expand = c(0.01, 0)) +
+    scale_x_continuous(limits = c(30, 90)) +
+    geom_segment(x = median.age, y = 0, xend = median.age, yend = upper.limit.plot, color = 'red', size = 0.8) +
+    labs(title = i)
+  
+  plot_list[[i]] = plot
+  
+  rm(data.plot,
+     median.age,
+     peak.max,
+     upper.limit.plot)
+}
+
+Age_distribution_IMPACT = plot_list$`Esophagogastric Cancer` / plot_list$`Pancreatic Cancer` / plot_list$`Colorectal Cancer` / plot_list$`Soft Tissue Sarcoma` / plot_list$`Prostate Cancer` / plot_list$Glioma
+ggsave_golden(plot = Age_distribution_IMPACT, filename = 'Figures/Age_Distribution_IMPACT.pdf', width = 12)
 
 
+#' look at the general trend between Y-chromosome loss and intact among age groups;
+i_without = IMPACT_data[!is.na(IMPACT_data$AGE_AT_SEQ_REPORTED_YEARS), ]
+Age_cohort = ggplot(i_without) + 
+  geom_histogram(aes(x = AGE_AT_SEQ_REPORTED_YEARS, color = Y_call, fill = Y_call), bins = 35, binwidth = 0.999, na.rm = T) +
+  scale_y_continuous(expand = c(0, 0)) +
+  scale_x_continuous(expand = c(0, 0),
+                     limits = c(0, 90),
+                     breaks = seq(0, 90, 30)) +
+  scale_fill_manual(values = c('Y_chrom_loss' = '#c31f21',
+                               'intact_Y_chrom' = '#4977b0'),
+                    labels = c('Y loss', 'Intact Y chromosome'),
+                    guide = guide_legend(direction = 'horizontal',
+                                         title = '',
+                                         label.theme = element_text(size = 12))) +
+  scale_color_manual(values = c('Y_chrom_loss' = '#c31f21',
+                               'intact_Y_chrom' = '#4977b0')) +
+  guides(color = FALSE) +
+                     
+  theme_minimal() +
+  theme(axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        axis.line.x = element_line(size = 0.4, color = 'black'),
+        panel.grid = element_blank(),
+        axis.text.x = element_text(size = 10, color = 'black'),
+        panel.border = element_rect(fill = NA, size = 0.6),
+        legend.position = c(0.1, 0.9)) +
+  labs(x = 'Age')
+  
+ggsave_golden(Age_cohort, filename = 'Figures/Age_Distribution_Y_loss.pdf', width = 9)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#' out
