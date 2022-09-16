8c790bcf1830f827c00b0890a22bf334aa35bf22
chris kreitzer <christoph.kreitzer93@gmx.at>
2022-09-16 11:29:32 +0200

    depth_breadth

--- PanCancer/Scripts/depth_breadth.R
+++ PanCancer/Scripts/depth_breadth.R
@@ -63,4 +63,122 @@ breath_out = data.table::rbindlist(breath_out)
 write.table(x = breath_out, file = '/home/kreitzec/Master/Data/breath_out.txt', sep = '\t', row.names = F)
 
 #' out
- 
+
+
+
+
+
+##-----------------
+## Downstream analysis
+##-----------------
+## 09/16/2022
+
+clean()
+gc()
+.rs.restartR()
+setup(working.path = '~/Documents/MSKCC/10_MasterThesis/')
+coverage = read.csv('~/Documents/MSKCC/10_MasterThesis/Data/01_Coverage_Depth/breath_out.txt', sep = '\t')
+coverage$Sample[which(unique(duplicated(coverage$Sample)))]
+
+n = length(unique(coverage$Sample))
+gene_coverage = data.frame()
+for(i in unique(coverage$gene)){
+  gene = i
+  fraction = length(unique(coverage$Sample[which(coverage$gene == i)])) / n
+  out = data.frame(gene = gene,
+                   fraction = fraction * 100)
+  gene_coverage = rbind(gene_coverage, out)
+}
+
+##-----------------
+## only include genes which 
+## are covered in at least 
+## 75% of the cohort
+##-----------------
+gene_coverage_final = gene_coverage[which(gene_coverage$fraction >= 74), ]
+gene_coverage_final = gene_coverage_final[order(gene_coverage_final$fraction, decreasing = T), ]
+gene_coverage_final$gene = factor(gene_coverage_final$gene, levels = gene_coverage_final$gene)
+source('~/Documents/MSKCC/10_MasterThesis/Scripts/plot_theme.R')
+
+coverage_IMPACT = ggplot(gene_coverage_final, aes(x = gene, y = fraction)) +
+  geom_bar(stat = 'identity') +
+  geom_hline(yintercept = seq(25, 75, 25), color = 'white') +
+  scale_y_continuous(expand = c(0,0), limits = c(0, 100)) +
+  theme_std(base_size = 14, base_line_size = 1) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = '', y = 'Genomic features covered\n by MSK-IMPACT assay [%]')
+
+ggsave(filename = '~/Documents/MSKCC/10_MasterThesis/Figures_original/Feature_Coverage_IMPACT.pdf', plot = coverage_IMPACT, width = 8)
+
+
+##-----------------
+## Stratify by gene panel
+##-----------------
+sample_ids = unique(coverage$Sample)
+panel = data.frame(sample = sample_ids,
+                   panel = NA)
+panel$panel = ifelse(grepl('IM3', panel$sample), 'IM3',
+                     ifelse(grepl('IM5', panel$sample), 'IM5',
+                            ifelse(grepl('IM6', panel$sample), 'IM6',
+                                   ifelse(grepl('IM7', panel$sample), 'IM7', NA))))
+                     
+
+IM3 = 1184
+IM5 = 4453
+IM6 = 13629
+IM7 = 3046
+
+gene_panel = data.frame()
+for(i in unique(coverage$gene)){
+  for(j in unique(coverage$panel)){
+    gene = i
+    if(j == 'IM3'){
+      fraction = (length(coverage$Sample[which(coverage$gene == i & coverage$panel == j)]) / IM3) * 100
+    } else if (j == 'IM5'){
+      fraction = (length(coverage$Sample[which(coverage$gene == i & coverage$panel == j)]) / IM5) * 100
+    } else if (j == 'IM6'){
+      fraction = (length(coverage$Sample[which(coverage$gene == i & coverage$panel == j)]) / IM6) * 100
+    } else {
+      fraction = (length(coverage$Sample[which(coverage$gene == i & coverage$panel == j)]) / IM7) * 100
+    }
+    
+    out = data.frame(gene = gene,
+                     panel = j,
+                     fraction = fraction)
+    gene_panel = rbind(gene_panel, out)
+  }
+}
+
+##-----------------
+## consider 17 elements from before
+##-----------------
+gene_panel = gene_panel[which(gene_panel$gene %in% gene_coverage_final$gene), ]
+gene_panel$gene = factor(gene_panel$gene, levels = gene_coverage_final$gene)
+gene_panel$panel = factor(gene_panel$panel, levels = c('IM3', 'IM5', 'IM6', 'IM7'))
+
+Feature_Panel = ggplot(gene_panel, aes(x = gene, y = fraction, fill = panel)) +
+  geom_bar(stat = 'identity', position = 'dodge') +
+  scale_fill_manual(values = c('IM3' = '#BABAB9',
+                               'IM5' = '#898999',
+                               'IM6' = '#5B5F75',
+                               'IM7' = '#3B455F')) +
+  geom_hline(yintercept = seq(25, 75, 25), color = 'white') +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 100)) +
+  theme_std(base_size = 14, base_line_size = 1) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = '', y = 'Genomic features covered\n by MSK-IMPACT assay [%]')
+
+ggsave(filename = '~/Documents/MSKCC/10_MasterThesis/Figures_original/Feature_Coverage_IMPACT_PANEL.pdf', plot = Feature_Panel, width = 8)
+
+
+
+
+
+
+
+
+
+
+
+
+
