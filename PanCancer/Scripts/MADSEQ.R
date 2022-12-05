6eae82b4ea4650ce4fedfdb862710794e9368f45
chris kreitzer <christoph.kreitzer93@gmx.at>
2022-12-05 18:16:55 +0100

    MADSEQ update

---
+++ PanCancer/Scripts/MADSEQ.R
@@ -0,0 +1,75 @@
+##----------------+
+## MADSEQ masterfile
+##----------------+
+
+source('~/MADSEQ/MADSEQ_helper.R')
+Masterfile = read.csv(file = '~/MADSEQ/IMPACT_dataFreeze_07.13.22.txt', sep = '\t')
+
+
+#' Path to file
+normal_bam = Masterfile$Normal_bam[2]
+target = ifelse(Masterfile$GENE_PANEL[2] == 'IMPACT341',
+                '~/MADSEQ/IMPACT341_b37_baits.bed', '~/MADSEQ/IMPACT410_b37_baits.bed')
+genome_assembly = 'hg19'
+
+
+#' get coverage
+target_gr = getCoverage(bam = normal_bam, 
+                        target_bed = target, 
+                        genome_assembly = 'hg19')
+
+target_gr = addchr(target_gr)
+gc = calculateGC(range = target_gr)
+
+if(length(gc) == length(target_gr)){
+  mcols(target_gr)$GC = gc
+}
+
+## filter out regions around gaps
+target_gr_deGAP = removeGap(target_gr,genome_assembly) 
+
+## filter out HLA regions
+target_gr_deHLA = removeHLA(target_gr_deGAP,genome_assembly)
+
+## filter out other highly polymorphic regions
+target_gr_deAQP = removeAQP(target_gr_deHLA,genome_assembly)
+
+## filter out regions overlap with repeats
+#target_gr_final = removeRE(target_gr_deAQP,genome_assembly)
+target_gr_final = target_gr_deAQP
+target_gr_final
+
+
+
+
+# prepareCoverageGC = function(target_bed, bam, genome_assembly = "hg19"){
+#   ## prepare coverage
+#   target_gr = getCoverage(bam=bam, target_bed=target_bed, 
+#                           genome_assembly = genome_assembly)
+#   
+#   ## calculate GC
+#   gc = calculateGC(range=target_gr, genome_assembly=genome_assembly)
+#   
+#   ## check that length of gc equals to length of target
+#   if(length(gc) == length(target_gr)){
+#     mcols(target_gr)$GC = gc
+#   }
+#   else stop(paste("with", length(target_gr), "target regions, only", 
+#                   length(gc), 
+#                   "GC content calculated.Please check your input.",
+#                   sep=" "))
+#   
+#   ## filter out regions around gaps
+#   target_gr_deGAP = removeGap(target_gr,genome_assembly) 
+#   
+#   ## filter out HLA regions
+#   target_gr_deHLA = removeHLA(target_gr_deGAP,genome_assembly)
+#   
+#   ## filter out other highly polymorphic regions
+#   target_gr_deAQP = removeAQP(target_gr_deHLA,genome_assembly)
+#   
+#   ## filter out regions overlap with repeats
+#   #target_gr_final = removeRE(target_gr_deAQP,genome_assembly)
+#   target_gr_final = target_gr_deAQP
+#   target_gr_final
+# }
---
+++ PanCancer/Scripts/MADSEQ_helper.R
@@ -0,0 +1,391 @@
+##----------------+
+## MADSEQ helper functions;
+##----------------+
+
+library(GenomicAlignments)
+library(GenomicFeatures)
+library(Rsamtools)
+library(rtracklayer)
+library(BSgenome)
+library(diffloop)
+
+
+## given a GRange object and bam file, 
+## calculate average coverage for each range
+calculateSubCoverage = function(range, bam){
+  ## read bam file from given ranges,
+  ## filter out duplicated reads, secondary reads and unmapped reads
+  ## exclude reads with mapQ==0
+  param = ScanBamParam(flag=scanBamFlag(isUnmappedQuery=FALSE, 
+                                        isSecondaryAlignment=FALSE, 
+                                        isDuplicate=FALSE),
+                       which=range,
+                       mapqFilter=1)
+  ## read alignment
+  sub_alignment = GenomicAlignments::readGAlignments(bam, param = param)
+  ## calculate coverage
+  cov = GenomicAlignments::coverage(sub_alignment)
+  cov = cov[range]
+  ## return average coverage for each region
+  round(mean(cov))
+}
+
+
+## given the path to targeted bed file and bam file, 
+## create a GRanges object containing coverage for each targeted region
+getCoverage = function(bam, target_bed, genome_assembly="hg19"){
+  
+  ## read in target bed table
+  target_gr = rtracklayer::import(target_bed)
+  if(nchar(seqlevels(target_gr)[1])>3){
+    seqlevels(target_gr,pruning.mode="coarse")=c("chr1","chr2","chr3","chr4","chr5",
+                                                 "chr6","chr7","chr8","chr9","chr10",
+                                                 "chr11","chr12","chr13","chr14",
+                                                 "chr15","chr16","chr17","chr18",
+                                                 "chr19","chr20","chr21","chr22",
+                                                 "chrX","chrY")
+  }
+  else{
+    seqlevels(target_gr,pruning.mode="coarse")=c("1","2","3","4","5",
+                                                 "6","7","8","9","10",
+                                                 "11","12","13","14",
+                                                 "15","16","17","18",
+                                                 "19","20","21","22",
+                                                 "X","Y")
+  }
+  target_gr = sort(target_gr)
+  # remove regions overlapped with REs
+  #target_gr = removeRE(target_gr,genome_assembly)
+  target_gr = removeGap(target_gr,genome_assembly)
+  target_gr = removeHLA(target_gr,genome_assembly)
+  target_gr = removeAQP(target_gr,genome_assembly)
+  nRegion = length(target_gr)
+  cat(paste(nRegion, "non-repeats regions from", length(seqlevels(target_gr)),
+            "chromosomes in the bed file.", sep=" "))
+  ## use helper function to calculate average coverage for each region, 
+  ## in order to handle large bam files, 
+  ## process 1000 regions at a time to reduce memory usage
+  message("calculating depth from BAM...")
+  depth = NULL
+  for (i in seq(1,nRegion,1000)){
+    ## report progress
+    if(i%%5000==1&i>1) cat(paste(i-1,"regions processed\n"))
+    end = ifelse(i+999>nRegion,nRegion,i+999)
+    sub_depth = calculateSubCoverage(target_gr[i:end],bam)
+    depth = c(depth, sub_depth)
+  }
+  ## see if number of depth equals to number of regions
+  if (length(depth) == nRegion) mcols(target_gr)$depth = depth
+  else stop(paste("with", nRegion, "target regions, only", 
+                  length(depth), "processed. Please check your input.",
+                  sep=" "))
+  target_gr
+}
+
+
+## helper function to prepare gc data
+## given targets as a GRanges object, calculate gc content for each range
+calculateGC = function(
+    range,
+    genome_assembly="hg19"){
+  genome = BSgenome::getBS(genome_assembly)
+  ## use alphabetFrequency function in biostring to calculate GC percent
+  message("calculating GC content...")
+  base_frequency = alphabetFrequency(BSgenomeViews(genome,range),
+                                     as.prob = TRUE)[,c("C","G")]
+  gc_content = apply(base_frequency,1,sum)
+  gc_content
+}
+
+## helper function to correct coverage by GC content
+## given a GRanges object, output corrected coverage
+correctGCBias = function(
+    object,
+    plot=FALSE){
+  ## correct coverage by GC content
+  ## convert GRanges object to frame
+  gc_depth = as.data.frame(object)
+  name = names(gc_depth)
+  
+  ## check if data has been quantile normalized
+  ## if data has been quantile normalized, following analysis is operated
+  ## on quantiled depth
+  quantiled = FALSE
+  if (is.element("quantiled_depth",name)){
+    ## exclude regions with 0 coverage
+    quantiled = TRUE
+    gc_depth = gc_depth[gc_depth$depth>0&gc_depth$quantiled_depth>0,]
+    names(gc_depth) = sub("quantiled_depth","coverage",name)
+  }
+  else{
+    gc_depth = gc_depth[gc_depth$depth>0,]
+    names(gc_depth) = sub("depth","coverage",name)
+  }
+  
+  ## round gc content to 0.001 increments
+  gc_depth = data.frame(gc_depth,round_gc=round(gc_depth$GC,3))
+  ## split data by GC content
+  split_gc = split(gc_depth,gc_depth$round_gc)
+  coverage_by_gc = sapply(split_gc,function(x)mean(x$coverage,na.rm=TRUE))
+  gc_coverage = data.frame(round_gc = as.numeric(names(coverage_by_gc)),
+                           mean_reads = coverage_by_gc)
+  ## fit coverage and GC content by loess
+  gc_coverage_fit = stats::loess(gc_coverage$mean_reads~gc_coverage$round_gc,
+                                 span=0.5)
+  ## the expected coverage is the mean of the raw coverage
+  expected_coverage = mean(gc_depth[,"coverage"])
+  
+  ## plot GC vs. raw reads plot
+  if(plot == TRUE){
+    plot(x = gc_coverage$round_gc, 
+         y = gc_coverage$mean_reads, 
+         pch = 16, col = "blue", cex = 0.6, 
+         ylim = c(0,1.5*quantile(gc_coverage$mean_reads,0.95,na.rm=TRUE)),
+         xlab = "GC content", ylab = "raw reads", 
+         main = "GC vs Coverage Before Norm",cex.main=0.8)
+    graphics::lines(gc_coverage$round_gc, 
+                    stats::predict(gc_coverage_fit, gc_coverage$round_gc), 
+                    col = "red", lwd = 2)
+    graphics::abline(h = expected_coverage, lwd = 2, col = "grey", lty = 3)
+  }
+  
+  ## correct reads by loess fit
+  normed_coverage = NULL
+  for (i in 1:24){
+    ## check if the coordinate is with "chr" or not
+    if(nchar(as.character(seqnames(object)@values[1]))>3) {
+      chr = paste("chr",i,sep="")
+      if (i == 23) chr = "chrX"
+      if (i == 24) chr = "chrY"
+    }
+    else{
+      chr = i
+      if (i == 23) chr = "X"
+      if (i == 24) chr = "Y"
+    }
+    tmp_chr = gc_depth[gc_depth$seqnames == chr,]
+    if(nrow(tmp_chr)==0) next
+    chr_normed = NULL
+    for (j in 1:nrow(tmp_chr)){
+      tmp_coverage = tmp_chr[j,"coverage"]
+      tmp_GC = tmp_chr[j,"GC"]
+      # predicted read from the loess fit
+      tmp_predicted = stats::predict(gc_coverage_fit, tmp_GC)
+      # calculate the error biased from expected
+      tmp_error = tmp_predicted - expected_coverage
+      tmp_normed = tmp_coverage - tmp_error
+      chr_normed = c(chr_normed, tmp_normed)
+    }
+    normed_coverage = c(normed_coverage,chr_normed)
+  }
+  gc_depth = cbind(gc_depth,normed_coverage = normed_coverage)
+  gc_depth = gc_depth[!is.na(gc_depth$normed_coverage),]
+  
+  ## calculate and plot GC vs coverage after normalization
+  split_gc_after = split(gc_depth,gc_depth$round_gc)
+  coverage_by_gc_after = sapply(split_gc_after,
+                                function(x)mean(x$normed_coverage,
+                                                na.rm=TRUE))
+  gc_coverage_after=data.frame(round_gc=
+                                 as.numeric(names(coverage_by_gc_after)),
+                               mean_reads=coverage_by_gc_after)
+  gc_coverage_fit_after = stats::loess(gc_coverage_after$mean_reads
+                                       ~gc_coverage_after$round_gc,span=0.5)
+  
+  ## plot GC vs coverage after normalization
+  if (plot == TRUE){
+    plot(x = gc_coverage_after$round_gc, 
+         y = gc_coverage_after$mean_reads, 
+         pch = 16, col = "blue", cex = 0.6, 
+         ylim = c(0,1.5*quantile(gc_coverage_after$mean_reads,0.95,
+                                 na.rm=TRUE)),
+         xlab = "GC content", ylab = "normalized reads", 
+         main = "GC vs Coverage After Norm",cex.main=0.8)
+    graphics::lines(gc_coverage_after$round_gc, 
+                    stats::predict(gc_coverage_fit_after, gc_coverage_after$round_gc),
+                    col = "red", lwd = 2)
+  }
+  
+  ## round normalized coverage to integer
+  gc_depth$normed_coverage = round(gc_depth$normed_coverage)
+  ## exclude regions with corrected coverage <0
+  gc_depth = gc_depth[gc_depth$normed_coverage>0,]
+  
+  ## convert gc_depth into a GRanges object
+  if (quantiled == TRUE){
+    res = GRanges(seqnames = Rle(gc_depth$seqnames), 
+                  ranges = IRanges(start=gc_depth$start,end=gc_depth$end),
+                  strand = rep("*",nrow(gc_depth)),
+                  depth = gc_depth$depth,
+                  quantiled_depth = gc_depth$coverage,
+                  GC = gc_depth$GC,
+                  normed_depth = gc_depth$normed_coverage)
+  }
+  else{
+    res = GRanges(seqnames = Rle(gc_depth$seqnames), 
+                  ranges = IRanges(start=gc_depth$start,end=gc_depth$end),
+                  strand = rep("*",nrow(gc_depth)),
+                  depth = gc_depth$coverage,
+                  GC = gc_depth$GC,
+                  normed_depth = gc_depth$normed_coverage)
+  }
+  res = res[!is.na(mcols(res)$normed_depth)]
+  res
+}
+
+
+## function to calculate mean coverage for each chromosome after normalization
+## and could plot out the coverage before and after normalization
+## input: a GRangesList object
+calculateNormedCoverage = function(
+    object,
+    plot=F){
+  if(nchar(seqlevels(object)[1])>3){
+    chr_name=c("chr1","chr2","chr3","chr4","chr5",
+               "chr6","chr7","chr8","chr9","chr10",
+               "chr11","chr12","chr13","chr14",
+               "chr15","chr16","chr17","chr18",
+               "chr19","chr20","chr21","chr22",
+               "chrX","chrY")
+  }
+  else{
+    chr_name = c("1","2","3","4","5","6","7","8","9","10","11","12",
+                 "13","14","15","16","17","18","19","20","21","22","X","Y")
+  }
+  nSample = length(object)
+  sample_name = names(object)
+  split_object = sapply(object,function(x)split(x,seqnames(x)))
+  
+  ## calculate average coverage for each chromosome after normalization
+  after_chr = NULL
+  for (i in 1:nSample){
+    sub_after_chr = sapply(split_object[[i]],
+                           function(x)mean(mcols(x)$normed_depth))
+    after_chr = rbind(after_chr,sub_after_chr[chr_name])
+  }
+  rownames(after_chr) = sample_name
+  after_chr = replace(after_chr,is.nan(after_chr),0)
+  
+  ## if plot requested, then plot 
+  if (plot == TRUE){
+    graphics::par(mfrow=c(ifelse(nSample>1,3,2),1))
+    ## calculate average coverage before normalization
+    before_chr = NULL
+    quantiled_chr = NULL
+    for (i in 1:nSample){
+      sub_before_chr = sapply(split_object[[i]],
+                              function(x)mean(mcols(x)$depth))
+      before_chr = rbind(before_chr,sub_before_chr[chr_name])
+      if (nSample>1){
+        sub_quantiled_chr = sapply(split_object[[i]],
+                                   function(x)
+                                     mean(mcols(x)$quantiled_depth))
+        quantiled_chr=rbind(quantiled_chr,sub_quantiled_chr[chr_name])
+      }
+    }
+    before_chr = replace(before_chr,is.nan(before_chr),0)
+    quantiled_chr = replace(quantiled_chr,is.nan(quantiled_chr),0)
+    ## plot
+    cols = sample(grDevices::colors(),nSample,replace = TRUE)
+    nChr = ncol(after_chr)
+    ## 1. plot raw coverage
+    plot(1:nChr,rep(1,nChr),type="n",
+         ylim=c(0.5*min(before_chr,na.rm=TRUE),
+                1.5*max(before_chr,na.rm=TRUE)),
+         xlab="chromosome",ylab="average coverage",
+         main = "raw data",xaxt="n")
+    graphics::axis(1,at=seq(1,nChr),chr_name[1:nChr],las=2)
+    for (i in 1:nSample){
+      graphics::lines(1:nChr,before_chr[i,],type="b",pch=16,col=cols[i])
+    }
+    graphics::legend("topright",sample_name,pch=16,col=cols,
+                     ncol=ceiling(nSample/5),cex=0.6)
+    
+    if(nSample>1){
+      ## 2. plot quantiled coverage
+      plot(1:nChr,rep(1,nChr),type="n",xaxt="n",
+           ylim=c(0.5*min(quantiled_chr,na.rm=TRUE),
+                  1.5*max(quantiled_chr,na.rm=TRUE)),
+           xlab="chromosome",ylab="average coverage",
+           main="quantile normalized")
+      graphics::axis(1,at=seq(1,nChr),chr_name[1:nChr],las=2)
+      for (i in 1:nSample){
+        graphics::lines(1:nChr,quantiled_chr[i,],type="b",
+                        pch=16,col=cols[i])
+      }
+      graphics::legend("topright",sample_name,pch=16,col=cols,
+                       ncol=ceiling(nSample/5),cex=0.6)
+    }
+    
+    ## 3. plot normed coverage
+    plot(1:nChr,rep(1,nChr),type="n",xaxt="n",
+         ylim=c(0.5*min(after_chr,na.rm=TRUE),1.5*max(after_chr,na.rm=TRUE)),
+         xlab="chromosome",ylab="average coverage",main="GC normalized")
+    graphics::axis(1,at=seq(1,nChr),chr_name[1:nChr],las=2)
+    for (i in 1:nSample){
+      graphics::lines(1:nChr,after_chr[i,],type="b",pch=16,col=cols[i])
+    }
+    graphics::legend("topright",sample_name,pch=16,col=cols,
+                     ncol=ceiling(nSample/5),cex=0.6)
+  }
+  after_chr
+}
+
+
+
+# path = system.file("gap","hg19_gap_gr.RDS",package="MADSEQ")
+# a = readRDS(path)
+# b = a[seqnames(a) != 'chrY']
+# y = a[seqnames(a) == 'chrY']
+# y = y[-c(2,3,4,5,6,7)]
+# y = y[-c(2,3,4,5,6,7,8)]
+# y = y[-2]
+# uu = c(b, y)
+
+removeGap = function(gr, genome){
+  
+    gap_gr = readRDS('~/MADSEQ/hg19_gap_gr.RDS')
+    ov = findOverlaps(gr, gap_gr)
+    
+    if(length(ov)==0){
+      res_degap = gr
+    } else 
+      res_degap = gr[-queryHits(ov)]
+    res_degap
+}
+
+## remove SNPs inside the HLA region on chr6
+# because of the variability of HLA regions, 
+# variant calling for this region is problematic most of the time, 
+# to keep a clean result, we will filter out SNPs called within this region
+# padded 1000kb up and downstream of HLA region
+
+removeHLA = function(gr, genome){
+  
+  HLA_gr = readRDS('~/MADSEQ/hg19_HLA_gr.RDS')
+  ov = findOverlaps(gr, HLA_gr)
+  
+  if(length(ov)==0){
+    res_deHLA = gr
+  }
+  else
+    res_deHLA = gr[-queryHits(ov)]
+  res_deHLA
+}
+
+## remove SNPs inside the AQP7 region on chr9
+# because of the variability of AQP7, 
+# variant calling for this region is problematic most of the time, 
+# to keep a clean result, we will filter out SNPs called within this region
+# padded 1000kb up and downstream of AQP7 region
+removeAQP = function(gr,genome){
+  AQP7_gr = readRDS('~/MADSEQ/hg19_AQP7_gr.RDS')
+  ov = findOverlaps(gr, AQP7_gr)
+  
+  if(length(ov)==0){
+    res_deAQP7 = gr
+  }
+  else
+    res_deAQP7 = gr[-queryHits(ov)]
+  res_deAQP7
+}
