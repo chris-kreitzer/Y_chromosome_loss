dc46ae4912772d1f271a15f7ffc0da9efb27a22a
chris kreitzer <christoph.kreitzer93@gmx.at>
2022-12-06 10:02:30 +0100

    madseq update

--- PanCancer/Scripts/MADSEQ.R
+++ PanCancer/Scripts/MADSEQ.R
@@ -9,40 +9,41 @@ Masterfile = Masterfile[1:2, ]
 
 #' Path to file
 mLOY = function(bams){
-  name = Masterfile$SAMPLE_ID[bams]
-  normal_bam = Masterfile$Normal_bam[bams]
-  target = ifelse(Masterfile$GENE_PANEL[bams] == 'IMPACT341',
-                  '~/MADSEQ/IMPACT341_b37_baits.bed',
-                  ifelse(Masterfile$GENE_PANEL[bams] == 'IMPACT410',
-                         '~/MADSEQ/IMPACT410_b37_baits.bed',
-                         ifelse(Masterfile$GENE_PANEL[bams] == 'IMPACT468',
-                                '~/MADSEQ/IMPACT468_b37_baits.bed', '~/MADSEQ/IMPACT505_b37_baits.bed')))
-  print(bam)
-  genome_assembly = 'hg19'
-  
-  target_gr = getCoverage(bam = normal_bam, 
-                          target_bed = target, 
-                          genome_assembly = genome_assembly)
-  
-  target_gr = addchr(target_gr)
-  gc = calculateGC(range = target_gr)
-  
-  if(length(gc) == length(target_gr)){
-    mcols(target_gr)$GC = gc
-  }
-  
-  target_gr_deGAP = removeGap(target_gr,genome_assembly) 
-  target_gr_deHLA = removeHLA(target_gr_deGAP,genome_assembly)
-  target_gr_deAQP = removeAQP(target_gr_deHLA,genome_assembly)
-  target_gr_final = target_gr_deAQP
-  
-  normalizeCoverage(object = target_gr_final, 
-                    control = NULL, 
-                    writeToFile = TRUE, 
-                    destination = './Normalized', 
-                    sampleID = name,
-                    plot = FALSE)
-  
+  try({
+    name = Masterfile$SAMPLE_ID[bams]
+    normal_bam = Masterfile$Normal_bam[bams]
+    target = ifelse(Masterfile$GENE_PANEL[bams] == 'IMPACT341',
+                    '~/MADSEQ/IMPACT341_b37_baits.bed',
+                    ifelse(Masterfile$GENE_PANEL[bams] == 'IMPACT410',
+                           '~/MADSEQ/IMPACT410_b37_baits.bed',
+                           ifelse(Masterfile$GENE_PANEL[bams] == 'IMPACT468',
+                                  '~/MADSEQ/IMPACT468_b37_baits.bed', '~/MADSEQ/IMPACT505_b37_baits.bed')))
+    print(bams)
+    genome_assembly = 'hg19'
+    
+    target_gr = getCoverage(bam = normal_bam, 
+                            target_bed = target, 
+                            genome_assembly = genome_assembly)
+    
+    target_gr = addchr(target_gr)
+    gc = calculateGC(range = target_gr)
+    
+    if(length(gc) == length(target_gr)){
+      mcols(target_gr)$GC = gc
+    }
+    
+    target_gr_deGAP = removeGap(target_gr,genome_assembly) 
+    target_gr_deHLA = removeHLA(target_gr_deGAP,genome_assembly)
+    target_gr_deAQP = removeAQP(target_gr_deHLA,genome_assembly)
+    target_gr_final = target_gr_deAQP
+    
+    normalizeCoverage(object = target_gr_final, 
+                      control = NULL, 
+                      writeToFile = TRUE, 
+                      destination = './Normalized', 
+                      sampleID = name,
+                      plot = FALSE)
+  })
 }
 
 sapply(1:nrow(Masterfile), function(x) mLOY(bams = x))
